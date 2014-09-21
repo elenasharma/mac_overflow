aa242c45975b69bf0a633e5bd3f2b91d4b6f6233
Nicholas Cantelmi <ncantelmi@Nicholass-MacBook-Air.local>
2014-09-21 00:30:31 -0700

    Put in some conditional logic for showing the signout link or login link based on if there is a current_user

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get '/answers/:id/downvote', to: "answers#downvote", as: "downvote_answer"
 
   # for BCrypt
-  get '/' => 'home#index'
+  get '/' => 'home#index', as: :home
   get "/log_in" => "sessions#new"
   post "/log_in" => "sessions#create"
   get "/log_out" => "sessions#destroy", as: :log_out

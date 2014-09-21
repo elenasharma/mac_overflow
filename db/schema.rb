56a913fd6b0eb45fcdee64cf83c45cf31708e8e4
Elena Sharma <elenasharma3@gmail.com>
2014-09-21 14:33:27 -0700

    database reset to resolve password salt issue

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,8 @@ ActiveRecord::Schema.define(version: 20140919223713) do
 
   create_table "users", force: true do |t|
     t.string   "username"
-    t.string   "password"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

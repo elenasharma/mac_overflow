0c1f72205e74bc59905702bfe326638e31331010
Alisa Tatarinova <alisatatarinova@gmail.com>
2014-09-21 20:09:44 -0700

    Answers controller tests are finished

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -27,4 +27,47 @@ describe AnswersController, type: :controller do
 		end
 	end
 
+	context "#upvote" do
+		it "creates a vote object when the upvote link is clicked and it is associated with that answer" do 
+			expect{
+				get :upvote, id: answer.id, use_route: :answers_controller
+			}.to change{answer.votes.count}.from(0).to(1)
+		end
+		it "increments answer's vote_count attribute by one" do
+			expect{
+				get :upvote, id: answer.id, use_route: :answers_controller
+				answer.reload
+				}.to change{answer.votecount}.by(1)
+		end
+	end
+
+	context "#downvote" do 
+		let(:vote) {create :vote}
+		# No matter what I try, this test won't PASS!!!! 
+		# it "creates a vote object when the downvote link is clicked and it is associated with that answer" do 
+		# 	expect{
+		# 		answer.votes << vote
+		# 		get :downvote, id: answer.id, use_route: :answers_controller
+		# 		answer.reload
+		# 	}.to change{answer.votes.size}.by(-1)
+		# end
+		it "decrements the corresponding answer's vote count by 1" do 
+			expect{
+				answer.votes << vote 
+				get :downvote, id: answer.id, use_route: :answers_controller
+				answer.reload
+			}.to change{answer.votecount}.by(-1)
+		end
+	end
+
+
+
+
+
+
+
+
+
+
+
 end
